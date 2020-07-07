e4ca322b754293ebe92d861db05cd5fe420bb58a
Hana <momenthana@kakao.com>
2020-07-07 22:51:18 +0900

    [-] Pagination component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export { default as Button } from './Button'
 export { default as Right } from './Right'
 export { default as Card } from './Card'
-export { default as Pagination } from './Pagination'
